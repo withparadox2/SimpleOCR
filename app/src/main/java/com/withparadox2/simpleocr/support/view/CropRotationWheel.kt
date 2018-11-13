612a3f5d155657e093b6420ad8896788f1c4e125
withparadox2 <withparadox2@gmail.com>
2018-11-13 21:04:11 +0800

    Better layout

--- app/src/main/java/com/withparadox2/simpleocr/support/view/CropRotationWheel.kt
+++ app/src/main/java/com/withparadox2/simpleocr/support/view/CropRotationWheel.kt
@@ -49,43 +49,44 @@ class CropRotationWheel(context: Context, attributeSet: AttributeSet) : FrameLay
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawColor(Color.BLACK)
 
         val angle = -2 * rotateDegree
         val delta = angle % DELTA_ANGLE
         val segments = Math.floor(angle / DELTA_ANGLE.toDouble()).toInt()
 
+        val centerY = height - dp2px(11)
+        val centerX = width / 2
         (0..15).forEach { i ->
             var paint = whitePaint
             var a = i
             if (a < segments || a == 0 && delta < 0)
                 paint = colorPaint
 
-            drawLine(canvas, a, delta, width, height, a == segments || a == 0 && segments == -1, paint)
+            drawLine(canvas, a, delta, centerX, centerY, a == segments || a == 0 && segments == -1, paint)
 
             if (i != 0) {
                 a = -i
                 paint = if (a > segments) colorPaint else whitePaint
-                drawLine(canvas, a, delta, width, height, a == segments + 1, paint)
+                drawLine(canvas, a, delta, centerX, centerY, a == segments + 1, paint)
             }
         }
 
         colorPaint.alpha = 255
 
         tempRect.left = (width - dp2px(2.5f)) / 2f
-        tempRect.top = (height - dp2px(22)) / 2f
+        tempRect.top = centerY - dp2px(11f)
         tempRect.right = (width + dp2px(2.5f)) / 2f
-        tempRect.bottom = (height + dp2px(22)) / 2f
+        tempRect.bottom = centerY + dp2px(11f)
         canvas.drawRoundRect(tempRect, dp2px(2f), dp2px(2f), colorPaint)
     }
 
-    private fun drawLine(canvas: Canvas, i: Int, delta: Float, width: Int, height: Int, center: Boolean, paint: Paint) {
+    private fun drawLine(canvas: Canvas, i: Int, delta: Float, centerX: Int, centerY: Int, center: Boolean, paint: Paint) {
         var drawPaint = paint
-        val radius = (width / 2.0f - dp2px(70)).toInt()
+        val radius = (centerX - dp2px(80))
 
         val angle = 90 - (i * DELTA_ANGLE + delta)
         val offset = (radius * Math.cos(Math.toRadians(angle.toDouble()))).toInt()
-        val x = width / 2 + offset
+        val x = centerX + offset
 
         val f = Math.abs(offset) / radius.toFloat()
         val alpha = Math.min(255, Math.max(0, ((1.0f - f * f) * 255).toInt()))
@@ -98,7 +99,7 @@ class CropRotationWheel(context: Context, attributeSet: AttributeSet) : FrameLay
         val w = if (center) 4 else 2
         val h = if (center) dp2px(16) else dp2px(12)
 
-        canvas.drawRect((x - w / 2).toFloat(), ((height - h) / 2).toFloat(), (x + w / 2).toFloat(), ((height + h) / 2).toFloat(), drawPaint)
+        canvas.drawRect((x - w / 2).toFloat(), (centerY - h / 2).toFloat(), (x + w / 2).toFloat(), (centerY + h / 2).toFloat(), drawPaint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
