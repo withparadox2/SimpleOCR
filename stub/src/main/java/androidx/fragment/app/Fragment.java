b12f7a35103829a11ea11086a1a14d812765e29a
withparadox2 <withparadox2@gmail.com>
2018-11-24 22:50:41 +0800

    Use stub class to generate the smallest template package

---
+++ stub/src/main/java/androidx/fragment/app/Fragment.java
@@ -0,0 +1,22 @@
+package androidx.fragment.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment {
+    final public Bundle getArguments() {
+        throw new RuntimeException("stub");
+    }
+
+    final public FragmentActivity getActivity() {
+        throw new RuntimeException("stub");
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        throw new RuntimeException("stub");
+    }
+}
