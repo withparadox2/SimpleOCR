1a795efe1ff50795a98b84f763b2fc0e483da588
withparadox2 <withparadox2@gmail.com>
2019-01-29 10:15:17 +0800

    Integrate bugly

--- app/src/main/java/com/withparadox2/simpleocr/App.kt
+++ app/src/main/java/com/withparadox2/simpleocr/App.kt
@@ -2,6 +2,7 @@ package com.withparadox2.simpleocr
 
 import android.app.Application
 import android.os.Handler
+import com.tencent.bugly.crashreport.CrashReport
 import com.withparadox2.simpleocr.support.template.preloadTemplates
 
 /**
@@ -19,5 +20,10 @@ class App : Application() {
         private val handler: Handler = Handler()
         fun post(action: Runnable) = handler.post(action)
         fun postDelayed(action: Runnable, millis : Long) = handler.postDelayed(action, millis)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        CrashReport.initCrashReport(applicationContext, "8864f2e0c7", BuildConfig.DEBUG)
     }
 }
